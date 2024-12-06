10b2dcf2b51d683c9e350dac07ea4ab51d19998d
Dylan Harty <me@dylanharty.com>
2024-12-06 15:30:15 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mkdocs-image-gallery-plugin",
-    version="1.0.0",
+    version="1.0.1",
     description="An MkDocs plugin to generate an image gallery from a folder of images",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
